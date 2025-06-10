33ada8901cfa75c60ca8361008057bf973b78f5d
Rudra-744 <rudragajjar744@gmail.com>
2025-06-10 15:49:15 +0530

    Initial commit Loader part done

--- script.js
+++ script.js
@@ -4,38 +4,49 @@ tl.from(".line h1", {
     y: 100,
     stagger: 0.25,
     opacity: 0,
-    duration: 0.8,
+    duration: 0.5,
     delay: 0.5,
+});
+
+tl.from(".number", {
+    opacity: 0,
+    onComplete: startCounter 
 });
 
 tl.from(".Now", {
   opacity: 0,
-  ease: "power1.inOut"
+  ease: "power2.inOut"
 });
 
- let h5 = document.querySelector(".number h5");
+function startCounter() {
+    let h5 = document.querySelector(".number h5");
     let count = 0;
-    setInterval(function () {
+    let interval = setInterval(function () {
       if (count < 100) {
-        h5.innerHTML = count++;
+        h5.textContent = count++;
       } else {
-        h5.innerHTML = count;
+        h5.textContent = count;
+        clearInterval(interval);
       }
-    }, 35);
+    }, 33);
+}
 
 tl.to(".loader", {
   opacity: 0,
-  duration: 0.5,
-  delay: 2,
+  duration: 0.3,
+  delay: 3.5,
 });
 
-tl.from('.page1',{
-    y:1600,
-    delay:0.3,
-    opacity:0,
-    duration:1,
-    ease: "expo.out"
-})
-tl.to('.loader',{
-    display:"none"
-})
+tl.from('.page1', {
+    y: 1600,
+    delay: 0.3,
+    opacity: 0,
+    duration: 2,
+    ease: "power4.out",
+    onComplete: function () {
+      tl.to('.loader', {
+          display: "none"
+      });
+    }
+});
+
